682a740a5451b7443141fe5c8ea21954839b03ff
bjzhujin <czzjcz@163.com>
2021-02-02 21:31:54 +0800

    Initial commit

---
+++ src/com/Boc/helloword/TextPrinter.java
@@ -0,0 +1,14 @@
+package com.Boc.helloword;
+
+public class TextPrinter {
+    public static void main(String[] args) {
+        System.out.println("您好！");
+        System.out.println(" 欢迎！");
+        System.out.println("努力学习");
+        System.out.println("加油！");
+        /*if (1 = 1) {
+            User user = new User();
+//            User us;
+        }*/
+    }
+}
